5066f4d04d7ef7c89b1c6754535cecd2190d81c5
Sandler Maciel <sandlergiovanni@gmail.com>
2025-10-18 15:45:06 -0300

    Initial commit for project

---
+++ TCAExemploGustavoCosme/TCAExemploGustavoCosme/TCAExemploGustavoCosmeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAExemploGustavoCosmeApp.swift
+//  TCAExemploGustavoCosme
+//
+//  Created by Sandler Maciel on 18/10/25.
+//
+
+import SwiftUI
+
+@main
+struct TCAExemploGustavoCosmeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
