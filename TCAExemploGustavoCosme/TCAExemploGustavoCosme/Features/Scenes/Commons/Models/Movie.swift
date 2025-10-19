5bcf129b3ab7dbb9531f058ef32801ce94e036f1
Sandler Maciel <sandlergiovanni@gmail.com>
2025-10-18 21:18:38 -0300

    Adding Model.

---
+++ TCAExemploGustavoCosme/TCAExemploGustavoCosme/Features/Scenes/Commons/Models/Movie.swift
@@ -0,0 +1,26 @@
+//
+//  Movie.swift
+//  TCAExemploGustavoCosme
+//
+//  Created by Sandler Maciel on 18/10/25.
+//
+
+struct Movie: Identifiable, Codable, Equatable {
+    let id: Int
+    let title: String
+    let overview: String?
+    let posterPath: String?
+    let backdropPath: String?
+    let releaseDate: String?
+    let voteAverage: Double?
+    let voteCount: Int?
+
+    enum CodingKeys: String, CodingKey {
+        case id, title, overview
+        case posterPath = "poster_path"
+        case backdropPath = "backdrop_path"
+        case releaseDate = "release_date"
+        case voteAverage = "vote_average"
+        case voteCount = "vote_count"
+    }
+}
