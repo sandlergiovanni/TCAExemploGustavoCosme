26ddf278a13e42e8a33bf16194fb6c6684239ddf
Sandler Maciel <sandlergiovanni@gmail.com>
2025-10-18 21:19:00 -0300

    Adding Movie response.

---
+++ TCAExemploGustavoCosme/TCAExemploGustavoCosme/Features/Scenes/Commons/Responses/MovieResponse.swift
@@ -0,0 +1,19 @@
+//
+//  MovieResponse.swift
+//  TCAExemploGustavoCosme
+//
+//  Created by Sandler Maciel on 18/10/25.
+//
+
+struct MovieResponse: Codable {
+    let page: Int
+    let results: [Movie]
+    let totalPages: Int
+    let totalResults: Int
+
+    enum CodingKeys: String, CodingKey {
+        case page, results
+        case totalPages = "total_pages"
+        case totalResults = "total_results"
+    }
+}
